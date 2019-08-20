c90fe42c0faa039e7ac87ce92aa1d18e1a9c769f
rahmanabir <abir.rahman1995@gmail.com>
2019-08-20 13:50:29 +0600

    Game Logic implemented

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,5 @@
+// @author : Abir Rahman
+
 import java.net.*;
 import java.io.*;
 
@@ -6,19 +8,32 @@ public class Client{
  private Socket socket            = null;
  private DataInputStream  input   = null;
  private DataOutputStream out     = null;
+ private DataInputStream in       =  null;
 
  // constructor takes input address and port number
  public Client(String address, int port){
      //Creating a new connection with server
      try{
          socket = new Socket(address, port);
-         System.out.println("Connected");
+         System.out.println("Connected to Rock Paper Scissors Server");
 
          // input from console/terminal 
          input  = new DataInputStream(System.in);
 
          // sends user input to server socket 
          out    = new DataOutputStream(socket.getOutputStream());
+         
+         in = new DataInputStream(
+                 new BufferedInputStream(socket.getInputStream()));
+         String computer = "";
+        	 try {
+        		 computer = in.readUTF();
+        		 System.out.println(computer);
+        	 }
+        	 catch(IOException i){
+                 System.out.println(i);
+             }
+        
      }
      catch(UnknownHostException u){
          System.out.println(u);
@@ -26,23 +41,69 @@ public class Client{
      catch(IOException i){
          System.out.println(i);
      }
+     
+     String name = "";
+     
+     while (!name.equals("START")){
+         try{
+             name = input.readLine();
+      
+            	 out.writeUTF(name);
+            	 
+            	 String inputMsg = "";
+              	 try {
+              		 inputMsg = in.readUTF();
+              		 System.out.println(inputMsg);
+              		
+              	 }
+              	 catch(IOException i){
+                       System.out.println(i);
+                 }
+              	 name = "START";
+              	 
+         }
+         
+         catch(IOException i){
+             System.out.println(i);
+         }
+         
+       
+     }
+     
 
      // string to store message from input 
      String line = "";
 
      // keep reading until "Exit" is input
-     while (!line.equals("Exit")){
+     while (!line.equals("EXIT")){
          try{
              line = input.readLine();
-             out.writeUTF(line);
+             if(line.equals("1") || line.equals("2") || line.equals("3")) {
+            	 out.writeUTF(line);
+            	 String msg = "";
+              	 try {
+              		msg = in.readUTF();
+              		 System.out.println(msg);
+              	 }
+              	 catch(IOException i){
+                       System.out.println(i);
+                 }
+             }
+             else {
+            	 System.out.println("Invalid input, try again!"); 
+             }
          }
          catch(IOException i){
              System.out.println(i);
          }
+         
      }
 
+     System.out.println("GAME OVER");
+     
      // closing the connection 
      try{
+    	 in.close();
          input.close();
          out.close();
          socket.close();
--- src/Server.java
+++ src/Server.java
@@ -1,22 +1,51 @@
+// @author : Abir Rahman
+
 import java.net.*;
 import java.io.*;
+import java.util.Random;
 
 public class Server{
  private Socket          socket   = null;
  private ServerSocket    server   = null;
  private DataInputStream in       =  null;
-
+ private DataOutputStream out     = null;
+ public String player ="";
+ public int rock = 1; 
+ public int paper = 2;
+ public int scissor = 3;
+ public String rules = "1 - ROCK, 2 - PAPER, 3 - SCISSORS, type EXIT to quit anytime!";
+ 
  // constructor takes TCP port number
  public Server(int port){
      // starts server and listens for a connection
      try{
          server = new ServerSocket(port);
-         System.out.println("Server started");
+         System.out.println("Rock Paper Scissor Game Server started");
 
-         System.out.println("Waiting for a client ...");
+         System.out.println("Waiting for a player ...");
 
          socket = server.accept();
-         System.out.println("Client accepted");
+         System.out.println("Player accepted");
+         
+         out = new DataOutputStream(socket.getOutputStream());
+         String msg = "Please enter your name"; 
+         out.writeUTF(msg);
+         in = new DataInputStream(
+                 new BufferedInputStream(socket.getInputStream()));
+         String playerName = "";
+         while(!playerName.equals("START")) {
+        	 try {
+        		 playerName = in.readUTF();
+        		 out.writeUTF(playerName + " Registered, Start game by entering the following "+ rules);
+        		 player = playerName; 
+        		 playerName ="START";
+        	 }
+        	 catch(IOException i){
+                 System.out.println(i);
+             }
+        	 //break;
+         }
+         
 
          // takes input from the client socket
          in = new DataInputStream(
@@ -25,25 +54,109 @@ public class Server{
          String line = "";
 
          // reads message from client until "Exit" is sent
-         while (!line.equals("Exit")){
+         while (!line.equals("EXIT")){
              try{
                  line = in.readUTF();
                  System.out.println(line); //Replace this line with rock paper scissor function 
-
+                 RockPaperScissor(line);	
              }
              catch(IOException i){
-                 System.out.println(i);
+            	 break;
+                 //System.out.println(i);
+                 //System.out.println("Problem 1"); //DELETE
+                 
              }
+            
          }
+         System.out.println("GAME OVER");
          System.out.println("Closing connection");
 
          // closing connection
          socket.close();
          in.close();
+         out.close();
      }
      catch(IOException i){
          System.out.println(i);
      }
+ }
+ 
+ public int randomGenerator() {
+	 Random rand = new Random();
+	 int randomNumber =rand.nextInt((3 - 1) + 1) + 1;
+	return randomNumber;
+ }
+ 
+ public void RockPaperScissor(String x) {
+	 
+	 int computer =  randomGenerator();
+	 int userMove = Integer.parseInt(x);
+	 if(computer == userMove) {
+		 try {
+			out.writeUTF(player + " tied with Computer");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 }
+	 else if( computer == 1 && userMove == 3) {
+		 //Computer wins 
+		 try {
+				out.writeUTF(player + "(SCISSORS) lost to the Computer(ROCK)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }
+	 
+	 else if( computer == 1 && userMove == 2) {
+		 //User wins
+		 try {
+				out.writeUTF(player + "(PAPER) won with the Computer(ROCK)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }
+	 else if(computer == 2  && userMove == 3) {
+		 //USer wins
+		 try {
+				out.writeUTF(player + "(SCISSOR) won with the Computer(PAPER)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }
+	 else if(computer == 2 && userMove == 1) {
+		 //computer wins
+		 try {
+				out.writeUTF(player + "(ROCK) lost to the Computer(PAPER)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }
+	 else if(computer == 3 && userMove == 1) {
+		 //User wins
+		 try {
+				out.writeUTF(player + "(ROCK) won with the Computer(SCISSOR)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	 }
+	 else if(computer == 3 && userMove == 2) {
+		 //Computer wins
+		 try {
+				out.writeUTF(player + "(PAPER) lost to the Computer(SCISSORS)");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	 }
+	 
+	 
  }
 
  // starting the server with credentials
